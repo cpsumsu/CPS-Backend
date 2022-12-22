7be4ca0ad18effed9b7ccce18acb7dc5e9f0c6cc
K4Lok <samkalok107@gmail.com>
2022-12-22 19:30:13 +0800

    add column filering on events api

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -21,7 +21,8 @@ class EventController extends Controller
 
         $value = Cache::remember("events".$pagination_num.$page_num, $this->cacheTime, function () use ($pagination_num) {
             return Event::with('eventType', 'leaders', 'organizes')
-            ->paginate($pagination_num);
+                        ->orderBy('date', 'desc')
+                        ->paginate($pagination_num, $columns = ['id', 'name', 'venue', 'date', 'image', 'event_type_id']);
         });
 
         return $value;
