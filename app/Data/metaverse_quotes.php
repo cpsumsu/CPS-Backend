dae0603beb716d5cbfa419b12fe70333567e01cd
ImK4Lok <samkalok107@gmail.com>
2023-03-25 13:14:03 +0800

    add quotes: zh-mo version

--- app/Data/metaverse_quotes.php
+++ app/Data/metaverse_quotes.php
@@ -35,7 +35,7 @@ return [
             "author" => "Dr. Philip Rosedale"
         ],
         [
-            "id" => 70,
+            "id" => 7,
             "quote" => "The metaverse is an opportunity to create a digital world that is both accessible and meaningful for everyone",
             "author" => "Cory Ondrejka"
         ],
@@ -54,5 +54,57 @@ return [
             "quote" => "The metaverse is here, and it’s not only transforming how we see the world but how we participate in it – from the factory floor to the meeting room.",
             "author" => "Satya Nadella"
         ],
-    ]
+    ],
+    "zh-mo" => [
+        [
+            "id" => 1,
+            "quote" => "在元宇宙中，一切皆有可能。",
+            "author" => "尼爾·史蒂芬森"
+        ],
+        [
+            "id" => 2,
+            "quote" => "協作的未來在元宇宙。",
+            "author" => "戴夫·沃特斯"
+        ],
+        [
+            "id" => 3,
+            "quote" => "最簡單的說法，元宇宙就是三維的互聯網。",
+            "author" => "艾德·格里格"
+        ],
+        [
+            "id" => 4,
+            "quote" => "元宇宙不是一個公司建立的東西，而是互聯網發展的下一章。",
+            "author" => "馬克·扎克伯格"
+        ],
+        [
+            "id" => 5,
+            "quote" => "元宇宙是創建沉浸式體驗和重塑未來的終極平台。",
+            "author" => "約翰·卡馬克"
+        ],
+        [
+            "id" => 6,
+            "quote" => "元宇宙是人們連接、創建和分享體驗的前所未有的機會。",
+            "author" => "菲利普·羅斯代爾"
+        ],
+        [
+            "id" => 7,
+            "quote" => "元宇宙是創建一個對每個人都具有意義和可達性的數字世界的機會。",
+            "author" => "科里·翁德里卡"
+        ],
+        [
+            "id" => 8,
+            "quote" => "今天人們不理解NFT、元宇宙和加密貨幣，就像1995年人們不理解網上購物一樣。",
+            "author" => "阿努傑·賈薩尼"
+        ],
+        [
+            "id" => 9,
+            "quote" => "在未來5年，我們將有效地從人們主要認為我們是一家社交媒體公司轉變為成為一家元宇宙公司。",
+            "author" => "馬克·扎克伯格"
+        ],
+        [
+            "id" => 10,
+            "quote" => "元宇宙已經來臨，它不僅正在改變我們看待世界的方式，還在從工廠車間到會議室參與改變。",
+            "author" => "薩提亞·納德拉"
+        ],
+    ] 
 ];
